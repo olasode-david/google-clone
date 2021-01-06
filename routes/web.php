0092c3bb4d0156112776a94e2dd60e4fd68997e6
david-olasode <60571573+olasode-david@users.noreply.github.com>
2021-01-06 01:52:33 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,25 +22,14 @@ Route::get('/search',function () {
     $term = request('search');
     $clear = str_replace(' ', '', $term);
 
-   $search =  json_decode(file_get_contents("https://www.googleapis.com/customsearch/v1?key=AIzaSyA24hTlywwYHDnPAEWwP0L8H9lFtPzWJEs&cx=188fe336301c1cdd9&q=$clear"), true);
-
-   //return $search['searchInformation']['formattedTotalResults'];
-
-    //AIzaSyA24hTlywwYHDnPAEWwP0L8H9lFtPzWJEs    API_KEY
-    //188fe336301c1cdd9   search engine id
-
+   $search =  json_decode(file_get_contents("https://www.googleapis.com/customsearch/v1?key=Your-Api-key&cx=search-engine-id&q=$clear"), true);
+    
     return view('pages\search',[
         'searchs' => $search,
         'searchName' => $term,
     ]);
     
 })->name('search');
-
-// Route::get('/searchs',function () {
-//     return view('pages\search',[
-//         'searchs' => request('search'),
-//     ]);
-// })->name('searchs');
 
 Auth::routes();
 
